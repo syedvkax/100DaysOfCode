bb7eb27f46a60c9d0a6ac94e4aa98e99e694e842
syedvkax <syed.vkax@gmail.com>
2022-01-12 00:07:38 +0500

    Day05

---
+++ Day05/avgheight.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+count = 0
+for student in student_heights:
+  student += student
+  count += 1
+
+avg_height = student / count
+print(f"Average height of student is: {avg_height}")
